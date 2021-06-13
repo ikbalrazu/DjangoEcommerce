b99f323f4f813d32fa103fec1219c9a6115c3625
iqbal <iqbalraju123@gmail.com>
2021-06-13 18:15:37 +0600

    updated in project settings

--- EcomProject/settings.py
+++ EcomProject/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'EcomProject.urls'
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'  
+#STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
