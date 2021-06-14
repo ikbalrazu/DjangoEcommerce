ecf9e16089b8801e780e2f00a73c3899af10913d
iqbal <iqbalraju123@gmail.com>
2021-06-14 22:25:43 +0600

    static files setting for heroku

--- EcomProject/settings.py
+++ EcomProject/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # General use templates & template tags (should appear first)
     'adminlte3',
+    'whitenoise.runserver_nostatic',
     # Optional: Django admin theme (must be before django.contrib.admin)
     'adminlte3_theme',
     'django.contrib.admin',
@@ -65,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'EcomProject.urls'
@@ -137,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-#STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
